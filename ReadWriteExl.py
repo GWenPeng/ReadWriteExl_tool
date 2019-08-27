dd1f30717ff11a3c51d1d4fc43a8a3dccba796da
guwenpeng <wenpeng.gu@huilianyi.com>
2019-08-27 16:11:16 +0800

    python 读取数据并拼接成dict类型数据，并加在第E列

---
+++ ReadWriteExl.py
@@ -0,0 +1,28 @@
+import  xlrd
+from xlutils.copy import copy
+def   ReadWriteExl(filename):
+        excel=xlrd.open_workbook(filename)
+
+        excel.sheet_names()
+        sheet = excel.sheet_by_index(1)
+        c=[]
+        c= sheet.row_values(1)
+        workbook = xlrd.open_workbook(filename) #打开原文件
+        workbooknew = copy(workbook)  #copy元文件
+        # 打开第二个sheet
+        ws = workbooknew.get_sheet(1)
+        for num in range(1,sheet.nrows):
+            # sheet.nrows 为最大行数 从第二行开始读入数据并写在第E列
+            c=sheet.row_values(num)
+            dict = {"Code": "", "Dept": "", "HaveDinner": "", "MarketType": ""}
+            dict["Code"] = c[0]
+            dict["Dept"]=c[1]
+            dict["HaveDinner"]=c[2]
+            dict["MarketType"]=c[3]
+
+            ws.write(num, 4, str(dict)) #在第二sheet E列第二行开始写入
+        workbooknew.save('HotelParameter.xls')
+
+
+if __name__ == '__main__':
+    ReadWriteExl("C://HotelParameter-zh-CN(1).xlsx")
