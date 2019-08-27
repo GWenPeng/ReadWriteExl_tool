8343ecea5f603d729958cd3a069e1dc16b66784f
guwenpeng <wenpeng.gu@huilianyi.com>
2019-08-27 16:34:35 +0800

    优化代码

--- ReadWriteExl.py
+++ ReadWriteExl.py
@@ -2,10 +2,11 @@ import  xlrd
 from xlutils.copy import copy
 def   ReadWriteExl(filename):
         excel=xlrd.open_workbook(filename)
-
         excel.sheet_names()
         sheet = excel.sheet_by_index(1)
         c=[]
+        Str='{"Code":"SUNGL","Dept":"","HaveDinner":"","MarketType":""},{"Code":"SUNCT","Dept":"","HaveDinner":"","MarketType":""},{"Code":"SUNDPT","Dept":"","HaveDinner":"","MarketType":""},{"Code":"SUNPKG","Dept":"","HaveDinner":"",MarketType:""},{"Code":"GetinHouse","Dept":"","HaveDinner":"","MarketType":""},{"Code":"Nights","Dept":"","HaveDinner":"","MarketType":""}'
+
         c= sheet.row_values(1)
         workbook = xlrd.open_workbook(filename) #打开原文件
         workbooknew = copy(workbook)  #copy元文件
@@ -20,9 +21,9 @@ def   ReadWriteExl(filename):
             dict["HaveDinner"]=c[2]
             dict["MarketType"]=c[3]
 
-            ws.write(num, 4, str(dict)) #在第二sheet E列第二行开始写入
+            ws.write(num, 4, '['+str(dict)+Str+']') #在第二sheet E列第二行开始写入
         workbooknew.save('HotelParameter.xls')
 
 
 if __name__ == '__main__':
-    ReadWriteExl("C://HotelParameter-zh-CN(1).xlsx")
+    ReadWriteExl("HotelParameter-zh-CN(1).xlsx")
